71f4b519a85d9e410fb8852e29bf028968e5a956
Abdigani Diriye <Rupert@Rupert-PC>
2012-12-11 00:33:48 -0500

    Added WebPageMiner DOM iterator

--- src/contentminer/WebPageMiner.java
+++ src/contentminer/WebPageMiner.java
@@ -2,9 +2,12 @@ package contentminer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 
 
 
@@ -24,6 +27,21 @@ public class WebPageMiner {
 		Document doc;
 		try {
 			doc = Jsoup.connect("http://research.microsoft.com/en-us/um/people/ryenw/index.html").get();
+			List<Node> tx = doc.childNodes();
+			
+			for(Node tn:tx){
+				
+				int size = tn.childNodes().size();
+				
+				for(Node tn1: tn.childNodes()){
+					System.out.println("--"+tn1.toString());
+					for(Node tn2: tn1.childNodes()){
+						System.out.println("--"+tn2.toString());
+					}
+				}
+				
+			
+			}
 			String title = doc.title();
 			String body = doc.body().text();
 			NamedEntityExtractor nee = new NamedEntityExtractor();
