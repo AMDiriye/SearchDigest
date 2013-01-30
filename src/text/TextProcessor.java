a153ce7942c283fe6276bc28d31d723204e6df48
Abdigani Diriye <abdigani.diriye@gmail.com>
2013-01-30 09:30:29 -0500

    Extended  text processor

--- src/text/TextProcessor.java
+++ src/text/TextProcessor.java
@@ -9,49 +9,68 @@ public class TextProcessor {
 	String[] money;
 	String[] dates;
 	String[] phoneNumbers;
-	
+
 	public TextProcessor(){}
 
 
 	public void process(String text){
-		processDate(text);
-		processMoney(text);
-		processPercentage(text);
+		//processPhoneNumber(text);
+		processEmail(text);
+//		processDate(text);
+	//	processMoney(text);
+		//processPercentage(text);
 	}
 
 
 	private void processPercentage(String text) {
-		Pattern p = Pattern.compile("[-]?[0-9]+[.]?[/]?[0-9]*[\\s]*([%]|(percent))");
-		Matcher m = p.matcher(text);
-		
+		Matcher m = matchPattern("[-]?[0-9]+[.]?[/]?[0-9]*[\\s]*([%]|(percent))",
+				text);
+
 		while (m.find()) {
-		  System.out.println("percentage "+m.group());
+			System.out.println("percentage "+m.group());
 		}	
 	}
 
 
 	private void processMoney(String text) {
-		Pattern p = Pattern.compile("[$|¥|£|€]?[\\d]{1,3}(,[\\s]?[\\d]{3})*(.[\\s]*[\\d]{2})?[\\s]*((pound)|(dollar)|(euro)|[$]|[¥]|[£]|[€])");
-		Matcher m = p.matcher(text);
-		
+		Matcher m = matchPattern("[$|¥|£|€]?[\\d]{1,3}(,[\\s]?[\\d]{3})*(.[\\s]*[\\d]{2})?[\\s]*" +
+				"((pound)|(dollar)|(euro)|[$]|[¥]|[£]|[€])", text);
+
 		while (m.find()) {
-		  System.out.println("money "+m.group());
+			System.out.println("money "+m.group());
 		}
 	}
 
 
 	private void processDate(String text) {
-		Pattern p = Pattern.compile("([0-9]{1,2}||\\d{4})-([a-zA-Z]{3}||\\d{2})-([0-9]{4}||\\d{2})");
-		Matcher m = p.matcher(text);
-		
+		Matcher m = matchPattern("^((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$", text);
+
 		while (m.find()) {
-		  System.out.println("percentage "+m.group());
+			System.out.println("Date "+m.group());
 		}	
 
 	}
 
+	private void processPhoneNumber(String text) {
+		Matcher m = matchPattern("\\d{3}-\\d{3}-\\d{4}", text);
 
+		while (m.find()) {
+			System.out.println("PhoneNumber "+m.group());
+		}
+	}
 
+	private void processEmail(String text){
+		Matcher m = matchPattern("\\b[\\w.%-]+@(.)+\\.[A-Za-z]{2,4}\\b", text);
 
+		while (m.find()) {
+			System.out.println("Email "+m.group());
+		};
+	}
 
+	private Matcher matchPattern(String regex, String text){
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(text);
+
+		return m;
+	}
 }
