ace7a64cc87d631f713e5a46c37f7bf2086ef673
Abdigani Diriye <abdigani.diriye@gmail.com>
2013-02-03 22:10:28 -0500

    Edited extraction files

--- src/contentalignment/Segment.java
+++ src/contentalignment/Segment.java
@@ -12,7 +12,7 @@ import contentminer.NamedEntity;
 import contentminer.Utilities;
 
 public class Segment {
-	
+
 	String text;
 	String stopWordLessText;
 	String processedTerms;
@@ -20,42 +20,42 @@ public class Segment {
 	int uniqueTerms;
 	List<String> cleanTextList;
 	Node node;
-	
+
 	public Segment(String text, Node node)
 	{
 		this.text = text;
 		this.node = node;
 		stopWordLessText = Utilities.removeStopWords(text);
 		size = stopWordLessText.split("[\\s]").length;
-		
+
 	    processedTerms = Utilities.stem(stopWordLessText);
 		cleanTextList = new ArrayList<String>();
 		cleanTextList.addAll(Arrays.asList(processedTerms.split("[\\s]")));
-		
+
 		HashSet set = new HashSet();
 		set.add(stopWordLessText);
 		uniqueTerms = set.size();
 	}
 
-	
+
 	public String getText()
 	{
 		return text;
 	}
-	
+
 	public List<String> getCleanTextList()
 	{
 		return cleanTextList;
 	}
-	
+
 
 	public String getProcessedText() 
 	{
 		return  processedTerms;
 	}
-	
 
-	
+
+
 	@Override
 	public String toString(){
 		return node.toString();
