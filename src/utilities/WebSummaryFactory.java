bde3ac4eb2ef185181b7bc4727450f8d6552ec18
AMDiriye <abdigani.diriye@gmail.com>
2013-04-06 19:05:11 -0400

    Webpage summary templates

---
+++ src/utilities/WebSummaryFactory.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import net.sf.classifier4J.summariser.*;
+
+public class WebSummaryFactory {
+
+	
+	//TODO: makePageSegmentations
+	//TODO: makePageSummarization
+	//TODO: makePageStructure
+	//TODO: makeRelatedPages
+	//TODO: makeMap
+	//TODO: makeForms
+	//TODO: makeQueryRepresentation
+	//TODO: makeWithinSiteSearch
+
+	
+	public static void main(String aegs[]){
+		SimpleSummariser s = new SimpleSummariser();
+		String input = "The VectorClassifier is an implementation of IClassifier that uses the vector space search algorithm. This algorithm is quite fast (compared to the Bayesian algorithm) and does not require training of non-matches. It also has the advantage that its match ratings (as returned by ther classify method) are fairly well distriubuted unlike the Bayesian Classifier which tended to return 0.99 or 0.01. This characteristic makes it ideally suited for categorization type tasks.";
+		String summ = s.summarise(input, 1);
+		System.out.println(summ);
+	}
+}
---
+++ src/websummaryelements/PageSummarization.java
@@ -0,0 +1,5 @@
+package websummaryelements;
+
+public class PageSummarization {
+
+}
